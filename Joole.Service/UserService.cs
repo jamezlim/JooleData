7b0f48df9753f913023fffedbf0b921090da2045
James Lim <limcheolhyun930421@gmail.com>
2020-04-28 19:52:48 -0400

    Add project files.

---
+++ Joole.Service/UserService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Joole.Repo;
+using Joole.Data;
+
+namespace Joole.Service
+{
+    public class UserService
+    {
+        public void AddUser(tblUser newUser)
+        {
+            UnitOfWork uow = new UnitOfWork(new JooleDataContext());
+            uow.GetUsersRepo.Add(newUser);
+            uow.SaveChanges();
+            uow.Dispose();
+        }
+    }
+}
---
+++ Joole/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Joole.Data;
+using Joole.Service;
+
+namespace Joole.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(tblUser user)
+        {
+            UserService serviceObj = new UserService();
+            serviceObj.AddUser(user);
+            return View("Login");
+        }
+
+
+   
+    }
+}

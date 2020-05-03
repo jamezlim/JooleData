5a3556554d3ae2e0d3acc911585541536e6a0c9f
James Lim <limcheolhyun930421@gmail.com>
2020-05-03 19:21:26 -0400

    5/3

---
+++ Joole.Service/SubCategoryService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Joole.Data;
+using Joole.Repo;
+
+namespace Joole.Service
+{
+    public class SubCategoryService
+    {
+        UnitOfWork _uow;
+
+        public SubCategoryService()
+        {
+            _uow = new UnitOfWork(new JooleDataContext());
+        }
+
+        public IEnumerable<tblSubCategory> GetSubCategoryRepo()
+        {
+            return _uow.GetSubCategoriesRepo.GetRepo();
+
+        }
+    }
+}
---
+++ Joole/Controllers/SearchController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Joole.Service;
+
+namespace Joole.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: Search
+        public ActionResult Search()
+        {
+            var selectList = new CategoryService().GetCategoryRepo();
+            ViewBag.CategoryName = selectList;
+            //ViewBag.CategoryName = new SelectList(selectList,"CategoryId","CategoryName");
+            return View();
+        }
+
+        public ActionResult SubmitSearch(int categoryId, int subCategoryId)
+        { 
+            return View("Products");
+        }
+
+        public ActionResult GetSubCategory(int categoryValue)
+        {
+            var subCategories = new SubCategoryService().GetSubCategoryRepo();
+            var result = (from s in subCategories where s.SubCategoryCategoryId == categoryValue select s).ToList();
+            return Json(result.Select(x=> new {SubCategoryId = x.SubCategoryId, SubCategoryName = x.SubCategoryName }), JsonRequestBehavior.AllowGet);
+        }
+
+
+    }
+}
